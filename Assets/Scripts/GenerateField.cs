fce77cd9af06a25dc7b0e8f48ed294f53ca90410
ken417 <esp.dir@gmail.com>
2019-06-28 17:09:15 +0900

    shader

--- Assets/Scripts/GenerateField.cs
+++ Assets/Scripts/GenerateField.cs
@@ -1,60 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class GenerateField : MonoBehaviour
 {
+    [Header("アセットとして書き出す"),SerializeField]
+    bool createAssets = false;
+
     [Header("生成時に使うマテリアル"),SerializeField]
     Material material = null;
 
     [Header("フラットシェーディング風メッシュにするか"), SerializeField]
     bool flatMesh = false;
 
-    [Header("フィールドの大きさ"),SerializeField]
+    [Header("フィールドの大きさ(セルの枚数)"),SerializeField]
     Vector2 fieldSize = new Vector2(10, 10);
+    Vector2 verticesNum;//頂点の数
 
-    [Header("起伏のなめらかさ"),SerializeField]
-    float relief = 15f;
-
-    [Header("高さの最大値"),SerializeField]
-    float maxHeight = 10;
-
-
+    [Header("x=起伏のなめらかさ,y=高さの最大値"), SerializeField]
+    Vector2[] perlinNoise;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(!material)
+        verticesNum = new Vector2(fieldSize.x + 1, fieldSize.y + 1);
+
+
+        GameObject field = CreatePlaneField();
+
+        for (int i = 0;i <  perlinNoise.Length; ++i)
+        {
+            field = AddPerlinNoise(field, perlinNoise[i].x, perlinNoise[i].y);
+        }
+
+        if (createAssets)
+        {
+            AssetDatabase.CreateAsset(field.GetComponent<MeshFilter>().sharedMesh, "Assets/Field.asset");
+            AssetDatabase.SaveAssets();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    GameObject CreatePlaneField()
+    {
+        if (!material)
         {
             material = new Material(Shader.Find("Standard"));
         }
 
-        GameObject obj = new GameObject();
-        MeshFilter filter = obj.AddComponent<MeshFilter>();
-        MeshRenderer renderer = obj.AddComponent<MeshRenderer>();
-        MeshCollider collider = obj.AddComponent<MeshCollider>();
+        GameObject planeField = new GameObject("Field");
+        MeshFilter filter = planeField.AddComponent<MeshFilter>();
+        MeshRenderer renderer = planeField.AddComponent<MeshRenderer>();
+        MeshCollider collider = planeField.AddComponent<MeshCollider>();
 
-        Vector2 verticesNum = new Vector2(fieldSize.x + 1, fieldSize.y + 1);
-
+        //頂点を入れる変数
         Vector3[] vertices = new Vector3[(int)(verticesNum.x * verticesNum.y)];
 
-        float seedX = Random.value * 100f;
-        float seedZ = Random.value * 100f;
+
+        //頂点を並べる
         for (int x = 0; x < verticesNum.x; ++x)
         {
             for (int z = 0; z < verticesNum.y; ++z)
             {
-                float xSample = (x + seedX) / relief;
-                float zSample = (z + seedZ) / relief;
-                vertices[(int)(x + z * verticesNum.x)] = new Vector3(x, Mathf.PerlinNoise(xSample, zSample) * maxHeight, z);
+                vertices[(int)(x + z * verticesNum.x)] = new Vector3(x - (verticesNum.x / 2), 0, z - (verticesNum.y / 2));
             }
         }
 
-
+        //三角形の数
         int triangleNum = (int)((verticesNum.x) * (fieldSize.y)) * 2;
 
+        //三角形を書く順番に頂点を入れる変数
         int[] triangles = new int[triangleNum * 3];
 
+        //頂点を三角形になるよう並べる
         for (int x = 0; x < fieldSize.x; ++x)
         {
             for (int y = 0; y < fieldSize.y; ++y)
@@ -69,30 +93,57 @@ public class GenerateField : MonoBehaviour
             }
         }
 
-
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
 
+        //法線計算
         mesh.RecalculateNormals();
-
-        if(flatMesh)
-        {
-            mesh = FlatMesh.ChangeFlatMesh(mesh);
-        }
 
         filter.sharedMesh = mesh;
         collider.sharedMesh = mesh;
         renderer.material = material;
 
+        return planeField;
     }
 
-    // Update is called once per frame
-    void Update()
+    GameObject AddPerlinNoise(GameObject field,float relief,float maxHeight)
     {
+        MeshFilter mf = field.GetComponent<MeshFilter>();
+        MeshCollider collider = field.GetComponent<MeshCollider>();
 
+        Vector3[] vertices = new Vector3[(int)(verticesNum.x * verticesNum.y)];
+
+        float seedX = Random.value * 100f;
+        float seedZ = Random.value * 100f;
+        for (int x = 0; x < verticesNum.x; ++x)
+        {
+            for (int z = 0; z < verticesNum.y; ++z)
+            {
+                float xSample = (x + seedX) / relief;
+                float zSample = (z + seedZ) / relief;
+                vertices[(int)(x + z * verticesNum.x)] = mf.sharedMesh.vertices[(int)(x + z * verticesNum.x)] + new Vector3(0, Mathf.PerlinNoise(xSample, zSample) * maxHeight, 0);
+            }
+        }
+
+        mf.sharedMesh.vertices = vertices;
+        mf.sharedMesh.RecalculateNormals();
+
+        collider.sharedMesh = mf.sharedMesh;
+
+        return field;
     }
 
+    GameObject MidpointDisplacementFractal(GameObject field, float maxHeight)
+    {
+        Vector3[] vertices = new Vector3[(int)(verticesNum.x * verticesNum.y)];
 
+        //フィールドの頂点
+        int center = Random.Range(0, (int)(fieldSize.x * fieldSize.y));
+
+        vertices[center] = new Vector3(0, maxHeight, 0);
+        int x = center % (int)verticesNum.x;
+        return field;
+    }
 
 }
---
+++ Assets/Shaders/FieldColor.shader
@@ -0,0 +1,59 @@
+﻿Shader "Custom/Unlit/FieldColor"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+								float4 color : COLOR;
+								float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+								if (v.vertex.y < 5)
+								{
+									o.color = float4(0, 154.0f/255.0f, 237.0f/255.0f, 1);
+								}
+								else
+								{
+									float hight = 40;
+									o.color = float4(v.vertex.y / hight, v.vertex.y / hight + 0.5f, v.vertex.y / hight, 1);
+								}
+								o.vertex = UnityObjectToClipPos(v.vertex);
+								return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return i.color;
+            }
+            ENDCG
+        }
+    }
+}
