43e4fa365a481298618d49fcda5385ee7ba379a6
Kentaro Hara <esp.dir@gmail.com>
2019-06-30 15:49:55 +0900

    マスクとかをつけた

--- Assets/Scripts/FieldGenerator.cs
+++ Assets/Scripts/FieldGenerator.cs
@@ -6,12 +6,20 @@ using UnityEditor;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]//自動でコンポーネントをアタッチしてくれる
 public class FieldGenerator : MonoBehaviour
 {
+    const int defaultResolution = 100;//基本resolution
     [Header("一辺の四角の枚数"), Range(1, 200), SerializeField]
-    int resolution = 100;
+    int resolution = defaultResolution;
     int currentResolution;
 
     Vector3 offset;
     Vector3 rotation;
+
+    [Header("パーリンノイズを使うかどうか"), SerializeField]
+    bool usePerlinNoise = true;
+
+    [Header("パーリンノイズのシード"), SerializeField]
+    Vector2 perlinNoiseSeed;
+
 
     [Header("周波数"), SerializeField]
     float frequency = 3f;
@@ -25,6 +33,27 @@ public class FieldGenerator : MonoBehaviour
     [Header("オクターブをどれだけ影響させるか"), Range(0f, 1f), SerializeField]
     float persistence = 0.5f;
 
+    [Header("最大高度"), Range(0f, 1f), SerializeField]
+    float maxHeight = 1;
+
+    [Header("最小高度"), Range(0f, 1f), SerializeField]
+    float minHeight = 0.4f;
+
+    [Header("最低高度にどれだけ元の高度をのせるか"), Range(0f, 1f), SerializeField]
+    float minHeightRewind = 0.15f;
+
+    [Header("サークルマスクの半径"), Range(0f, 100f), SerializeField]
+    float cercleMaskRadius = 0;
+
+    [Header("マスクの影響レベル"), Range(-0.5f, 0.5f), SerializeField]
+    float cercleMaskAmount = 0;
+
+    [Header("エッジマスクの幅"), Range(0f, 100f), SerializeField]
+    float edgeMaskWidth = 0;
+
+    [Header("マスクの影響レベル"), Range(-0.5f, 0.5f), SerializeField]
+    float edgeMaskAmount = 0;
+
     [Header("山の色"), SerializeField]
     Gradient coloring;
 
@@ -33,6 +62,9 @@ public class FieldGenerator : MonoBehaviour
 
     [Header("山の色位置"), SerializeField]
     float colorPos = 0;
+
+    float minVertices = 1;
+    float maxVertices = 0;
 
 
     Mesh _mesh;
@@ -45,10 +77,9 @@ public class FieldGenerator : MonoBehaviour
     }
 
     Vector3[] vertices;
+    Vector3[] verticesMask;
     Vector3[] normals;
     Color[] colors;
-
-    Vector2 seed;
 
     void OnEnable()//コンポーネントがアクティブになったときに呼ばれる
     {
@@ -57,10 +88,8 @@ public class FieldGenerator : MonoBehaviour
             _mesh = new Mesh();
             _mesh.name = "Surface Mesh";
             GetComponent<MeshFilter>().mesh = _mesh;
+            GetComponent<MeshRenderer>().material = new Material(Shader.Find("Custom/Surface Shader"));
         }
-        seed.x = Random.value;
-        seed.y = Random.value;
-        GetComponent<MeshRenderer>().material = new Material(Shader.Find("Custom/Surface Shader"));
         Refresh();
     }
 
@@ -76,7 +105,12 @@ public class FieldGenerator : MonoBehaviour
         {
             CreateGrid();
         }
-        AddPerlinNoise();
+        AddMask();
+        if(usePerlinNoise)
+        {
+            AddPerlinNoise();
+        }
+        _mesh.RecalculateNormals();
     }
 
     void AddPerlinNoise()
@@ -87,18 +121,26 @@ public class FieldGenerator : MonoBehaviour
             {
                 float amplitude = 1f;
                 float range = 1f;
-                float sample = 0;
+                float sample = verticesMask[v].y;
                 float freq = frequency;
                 for (int o = 0; o< octaves;++o)
                 {
-                    float xSample = ((float)x/ resolution + seed.x ) * freq;
-                    float zSample = ((float)z/ resolution + seed.y ) * freq;
-                    sample += Mathf.PerlinNoise(xSample, zSample)* amplitude;
+                    float xSample = ((float)x/ resolution + perlinNoiseSeed.x) * freq;
+                    float zSample = ((float)z/ resolution + perlinNoiseSeed.y) * freq;
+                    sample += Mathf.PerlinNoise(xSample, zSample)* amplitude * maxHeight;
+
                     freq *= lacunarity;
                     amplitude *= persistence;
                     range += amplitude;
                 }
                 sample /= range;
+
+                //高度の低い場所の高さを上げる
+                if (sample < minHeight)
+                {
+                    sample = minHeight - (minHeight-sample) * minHeightRewind;
+                }
+
                 vertices[v].y = sample;
                 colors[v] = coloring.Evaluate(sample*colorWidth+(colorPos/10));
             }
@@ -106,8 +148,67 @@ public class FieldGenerator : MonoBehaviour
 
         _mesh.vertices = vertices;
         _mesh.colors = colors;
-        _mesh.RecalculateNormals();
     }
+
+    void AddMask()
+    {
+        verticesMask = vertices;
+        int p = resolution / 2;
+        float r = cercleMaskRadius * ((float)resolution / defaultResolution);
+        r *= r;
+        for (int v = 0,z = 0; z <= resolution; z++)
+        {
+            for (int x = 0; x <= resolution; x++, v++)
+            {
+                verticesMask[v].y = 0f;
+
+                //サークルマスク
+                if ((x - p) * (x - p) + (z - p) * (z - p) < r)
+                {
+                    verticesMask[v].y = (r - ((x - p) * (x - p) + (z - p) * (z - p))) * (cercleMaskAmount / r);
+                }
+
+                //エッジマスク
+                float emWidth = edgeMaskWidth * ((float)resolution / defaultResolution);
+                if (emWidth > z|| (resolution - emWidth) <z || emWidth > x || (resolution - emWidth) < x)
+                {
+                    float minZ = z;
+                    if ((resolution - z) < z) { minZ = (resolution - z); }
+                    float minX = x;
+                    if ((resolution - x) < x) { minX = (resolution - x); }
+
+
+                    if (minZ < minX)
+                    {
+                        if ((resolution - emWidth) < z)
+                        {
+                            verticesMask[v].y = (emWidth - (resolution - z)) * edgeMaskAmount;
+                        }
+                        else if (emWidth > z)
+                        {
+                            verticesMask[v].y = (emWidth - z) * edgeMaskAmount;
+                        }
+                    }
+                    else
+                    {
+                        if ((resolution - emWidth) < x)
+                        {
+                            verticesMask[v].y = (emWidth - (resolution - x)) * edgeMaskAmount;
+                        }
+                        else if (emWidth > x)
+                        {
+                            verticesMask[v].y = (emWidth - x) * edgeMaskAmount;
+                        }
+                    }
+                }
+
+                colors[v] = coloring.Evaluate(verticesMask[v].y * colorWidth+(colorPos/10));
+            }
+        }
+        _mesh.vertices = verticesMask;
+        _mesh.colors = colors;
+    }
+
 
     void CreateGrid()
     {
@@ -147,6 +248,36 @@ public class FieldGenerator : MonoBehaviour
             }
         }
         _mesh.triangles = triangles;
+    }
+
+    void PositionAdjustment()
+    {
+        for (int v = 0, z = 0; z <= resolution; z++)
+        {
+            for (int x = 0; x <= resolution; x++, v++)
+            {
+                if (minVertices > _mesh.vertices[v].y)
+                {
+                    minVertices = _mesh.vertices[v].y;
+                }
+                if (maxVertices < _mesh.vertices[v].y)
+                {
+                    maxVertices = _mesh.vertices[v].y;
+                }
+            }
+        }
+        for (int v = 0, z = 0; z <= resolution; z++)
+        {
+            for (int x = 0; x <= resolution; x++, v++)
+            {
+                float point = minVertices + maxVertices;
+                if(point != 0)
+                {
+                    vertices[v].y = _mesh.vertices[v].y - (point / 2);
+                }
+            }
+        }
+        _mesh.vertices = vertices;
     }
 }
 
